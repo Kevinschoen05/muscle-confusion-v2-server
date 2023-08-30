17276a17914cfd3e09d4a8d9895c86e9a2480c41
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-08-29 21:04:15 -0400

    updated model and basic CRUD functions

--- controllers/userInboxController.js
+++ controllers/userInboxController.js
@@ -1,5 +1,51 @@
 const UserInbox = require("../models/userInbox");
 
 module.exports = class UserInboxAPI {
+   static async createMessage(senderUserID, receiverUserID, messageType, content) {
+        try {
+            const newMessage = new Message({
+                senderUserID,
+                receiverUserID,
+                messageType,
+                content
+            });
+    
+            const savedMessage = await newMessage.save();
+            return savedMessage;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    // Get messages for a specific user's inbox
+    static async  getInboxMessages(userID) {
+        try {
+            const messages = await Message.find({ receiverUserID: userID }).sort({ timeStamp: -1 });
+            return messages;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    // Update a message (for example, mark it as read)
+    static async  updateMessage(messageID, updates) {
+        try {
+            const updatedMessage = await Message.findByIdAndUpdate(messageID, updates, { new: true });
+            return updatedMessage;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
+    // Delete a message
+    static async  deleteMessage(messageID) {
+        try {
+            const deletedMessage = await Message.findByIdAndDelete(messageID);
+            return deletedMessage;
+        } catch (error) {
+            throw error;
+        }
+    }
+    
 
 }
