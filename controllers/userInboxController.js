16bf528f73ba2962869af8a9cf06c709306e1c1e
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-09-27 15:43:05 -0400

    update to the inbox controller

--- controllers/userInboxController.js
+++ controllers/userInboxController.js
@@ -3,7 +3,7 @@ const UserInbox = require("../models/userInbox");
 module.exports = class UserInboxAPI {
    static async createMessage(senderUserID, receiverUserID, messageType, content) {
         try {
-            const newMessage = new Message({
+            const newMessage = new UserInbox({
                 senderUserID,
                 receiverUserID,
                 messageType,
@@ -20,7 +20,7 @@ module.exports = class UserInboxAPI {
     // Get messages for a specific user's inbox
     static async  getInboxMessages(userID) {
         try {
-            const messages = await Message.find({ receiverUserID: userID }).sort({ timeStamp: -1 });
+            const messages = await UserInbox.find({ receiverUserID: userID }).sort({ timeStamp: -1 });
             return messages;
         } catch (error) {
             throw error;
@@ -30,7 +30,7 @@ module.exports = class UserInboxAPI {
     // Update a message (for example, mark it as read)
     static async  updateMessage(messageID, updates) {
         try {
-            const updatedMessage = await Message.findByIdAndUpdate(messageID, updates, { new: true });
+            const updatedMessage = await UserInbox.findByIdAndUpdate(messageID, updates, { new: true });
             return updatedMessage;
         } catch (error) {
             throw error;
@@ -40,7 +40,7 @@ module.exports = class UserInboxAPI {
     // Delete a message
     static async  deleteMessage(messageID) {
         try {
-            const deletedMessage = await Message.findByIdAndDelete(messageID);
+            const deletedMessage = await UserInbox.findByIdAndDelete(messageID);
             return deletedMessage;
         } catch (error) {
             throw error;
