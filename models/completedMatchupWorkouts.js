ebd1e97f63f848600d65d38bfe4e65228b9dad58
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-10-16 14:55:18 -0400

    new model to capture completed matchup workouts

---
+++ models/completedMatchupWorkouts.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+const advanced = require("dayjs/plugin/advancedFormat");
+
+dayjs.extend(timezone);
+dayjs.extend(utc);
+dayjs.extend(advanced);
+dayjs.tz.setDefault("America/New_York");
+
+const completedMatchupWorkoutSchema = mongoose.Schema({
+  workoutID: String,
+  workoutTitle: String,
+  userWorkoutData: [
+    {
+      userID: String,
+      workoutDuration: String,
+      totalVolume: Number,
+      completionDate: {
+        type: Date,
+        default: () => dayjs().tz().format("YYYY-MM-DDTHH:mm:ss[Z]"),
+      },
+      exercises: {
+        type: Array,
+      },
+      externalities: {
+        type: Array,
+      },
+    },
+  ],
+});
+module.exports = mongoose.model(
+  "CompletedMatchupWorkout",
+  completedMatchupWorkoutSchema
+);
