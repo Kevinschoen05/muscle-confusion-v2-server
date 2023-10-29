6bdac0d4148a9b9dcaa1c1e8dad0d71a3759298a
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-10-28 20:48:55 -0400

    one more try

--- controllers/matchupWorkoutController.js
+++ controllers/matchupWorkoutController.js
@@ -1,14 +1,15 @@
 const MatchupWorkout = require("../models/MatchupWorkouts");
 
-export default class MatchupWorkoutAPI {
-    static async createMatchupWorkout(req, res) {
-      const newWorkout = req.body;
-      try {
-        await MatchupWorkout.create(newWorkout);
-        res.status(201).json({ message: "New Matchup Message Created Successfully" });
-      } catch (err) {
-        res.status(400).json({ message: err.message });
-      }
+module.exports = class MatchupWorkoutAPI {
+  static async createMatchupWorkout(req, res) {
+    const newWorkout = req.body;
+    try {
+      await MatchupWorkout.create(newWorkout);
+      res
+        .status(201)
+        .json({ message: "New Matchup Message Created Successfully" });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
     }
-  };
-  
+  }
+};
