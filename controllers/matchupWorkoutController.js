616bbe0e0c1cb71934d35c3cb21f4e7148196656
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-11-12 15:44:51 -0500

    return Id when creating new matchup workout

--- controllers/matchupWorkoutController.js
+++ controllers/matchupWorkoutController.js
@@ -4,10 +4,13 @@ module.exports = class MatchupWorkoutAPI {
   static async createMatchupWorkout(req, res) {
     const newWorkout = req.body;
     try {
-      await MatchupWorkout.create(newWorkout);
+      const createdWorkout = await MatchupWorkout.create(newWorkout);
       res
         .status(201)
-        .json({ message: "New Matchup Message Created Successfully" });
+        .json({
+          message: "New Matchup Message Created Successfully",
+          _id: createdWorkout._id,
+        });
     } catch (err) {
       res.status(400).json({ message: err.message });
     }
