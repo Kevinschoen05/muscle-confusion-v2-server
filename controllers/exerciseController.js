3903604e35664e8429b4811628a6a38841296938
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-12-19 22:10:33 -0500

    logic and route for generating random workout

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -23,7 +23,7 @@ module.exports = class ExerciseAPI {
   }
 
   static async fetchExerciseByExerciseId(req, res) {
-    const exerciseID = req.params.exerciseID
+    const exerciseID = req.params.exerciseID;
     try {
       const exercise = await Exercise.find({
         _id: exerciseID,
@@ -31,6 +31,72 @@ module.exports = class ExerciseAPI {
       res.status(200).json(exercise);
     } catch (err) {
       res.status(404).json({ message: err.message });
+    }
+  }
+
+  static async getExercisesForRandomWorkout(req, res) {
+    try {
+      const desiredExerciseCount = parseInt(req.query.desiredExerciseCount);
+      const minSets = parseInt(req.query.minSets);
+      const maxSets = parseInt(req.query.maxSets);
+      const minReps = parseInt(req.query.minReps);
+      const maxReps = parseInt(req.query.maxReps);
+      const muscleGroups = req.query.muscleGroups
+        ? req.query.muscleGroups.split(",")
+        : [];
+
+      let query = {};
+      if (muscleGroups.length > 0) {
+        query.primaryMuscleGroup = { $in: muscleGroups };
+      }
+
+      const allExercises = await Exercise.find(query);
+
+      if (allExercises.length < desiredExerciseCount) {
+        return res
+          .status(400)
+          .json({
+            message:
+              "Not enough exercises available for the selected muscle groups.",
+          });
+      }
+
+      const shuffledExercises = allExercises.sort(() => 0.5 - Math.random());
+      const selectedExercises = shuffledExercises.slice(
+        0,
+        desiredExerciseCount
+      );
+
+      const formattedExercises = selectedExercises.map((exercise) => {
+        const targetSets =
+          Math.floor(Math.random() * (maxSets - minSets + 1)) + minSets;
+
+        return {
+          exerciseName: exercise.exerciseName,
+          targetSets: targetSets,
+          sets: Array.from({ length: targetSets }, (_, index) => {
+            const targetReps =
+              Math.floor(Math.random() * (maxReps - minReps + 1)) + minReps;
+            return {
+              index: index + 1,
+              target_reps: targetReps,
+              actual_reps: 0,
+              target_weight: 0,
+              actual_weight: 0,
+              completed: false,
+              success: false,
+            };
+          }),
+          id: exercise._id,
+          primaryMuscleGroup: exercise.primaryMuscleGroup,
+          secondaryMuscleGroups: exercise.secondaryMuscleGroups,
+          exerciseType: exercise.exerciseType,
+        };
+      });
+
+      res.status(200).json(formattedExercises);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
     }
   }
 
