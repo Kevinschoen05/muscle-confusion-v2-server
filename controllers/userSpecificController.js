f72736c322ace5aedef88bfb05e062c989ac9bf3
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2024-02-17 15:54:00 -0500

    routing+functions for  getting/adding targetWeight

--- controllers/userSpecificController.js
+++ controllers/userSpecificController.js
@@ -207,6 +207,25 @@ module.exports = class UserSpecificAPI {
     }
   }
 
+  static async getUserTargetWeights(req, res) {
+    const userID = req.params.userID;
+
+    try {
+      // Find the user by userID and only return the weights field
+      const user = await User.findOne({ userID: userID }, "targetWeights");
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // Return the user's weights entries
+      res.status(200).json(user.weights);
+    } catch (err) {
+      // Handle possible errors
+      res.status(500).json({ message: err.message });
+    }
+  }
+
   static async addUserWeight(req, res) {
     const newWeight = {
       weight: req.body.weight, // Assuming the request body contains a weight field
@@ -218,6 +237,24 @@ module.exports = class UserSpecificAPI {
       await User.findOneAndUpdate(
         { userID: userID }, // Find the user by userID
         { $push: { weights: newWeight } } // Add the new weight entry to the weights array
+      );
+      res.status(200).json({ message: "Weight Record Added Successfully" });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  }
+
+  static async addUserTargetWeight(req, res) {
+    const newWeight = {
+      weight: req.body.weight, // Assuming the request body contains a weight field
+      date: new Date(), // Optionally capture the date; it will default to now if not provided
+    };
+    const userID = req.params.userID;
+
+    try {
+      await User.findOneAndUpdate(
+        { userID: userID }, // Find the user by userID
+        { $push: { targetWeights: newWeight } } // Add the new weight entry to the weights array
       );
       res.status(200).json({ message: "Weight Record Added Successfully" });
     } catch (err) {
