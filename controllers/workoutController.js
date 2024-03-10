ff07d67b9e23386792b8752885ea38a8d62efe12
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2024-03-09 22:20:26 -0500

    new routing and function to add users to workout

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,4 +1,5 @@
 const Workout = require("../models/workouts");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports = class WorkoutAPI {
   static async fetchAllWorkouts(req, res) {
@@ -34,16 +35,46 @@ module.exports = class WorkoutAPI {
   static async updateWorkoutByWorkoutID(req, res) {
     const updatedWorkout = req.body.updatedFinalWorkout;
     const workoutID = req.body._id;
+
     try {
       await Workout.findOneAndUpdate(
         {
           _id: workoutID,
         },
         { $set: { exercises: updatedWorkout } }
-      )
-      res.status(200).json({message: "Workout Updated Successfully"});
-    } 
-    catch (err) {
+      );
+      res.status(200).json({ message: "Workout Updated Successfully" });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  }
+
+  static async addUserToWorkout(req, res) {
+    const workoutID = req.body.workoutID;
+    const userID = req.body.userID;
+    console.log(`Received workoutID: ${workoutID}, userID: ${userID}`);
+
+    try {
+      const result = await Workout.findOneAndUpdate(
+        { _id: ObjectId(workoutID) }, // Mongoose automatically handles the conversion to ObjectId.
+        { $addToSet: { users: userID } }, // Use $addToSet as confirmed to be working
+        { new: true } // Returns the modified document
+      );
+      console.log(result); // See what the database operation returns
+
+      if (result) {
+        res
+          .status(200)
+          .json({
+            message: "User added to workout successfully",
+            data: result,
+          });
+      } else {
+        // If no document was found and updated, return a different message
+        res.status(404).json({ message: "Workout not found" });
+      }
+    } catch (err) {
+      console.error(err); // More detailed error logging
       res.status(400).json({ message: err.message });
     }
   }
